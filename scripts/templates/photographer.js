b78e2936944161471d3cc9ec1d05454822da2e76
badgohan <kaori972@hotmail.fr>
2023-12-13 15:57:30 +0100

    Ajout des liens et du style sur la page d'acceuil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,26 +1,32 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        const lien = document.createElement('a');
+        lien.setAttribute("href", `${id}`);
+        lien.classList.add('photographer_link');
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        const localisation = document.createElement('p');
+        const localisation = document.createElement('h3');
         localisation.textContent = `${city}, ${country}`;
-        const accroche = document.createElement('p');
+        const accroche = document.createElement('blockquote');
         accroche.textContent = tagline;
         const tarif = document.createElement('p');
-        tarif.textContent = `${price}€/jour`
+        tarif.textContent = `${price}€/jour`;
+
+        lien.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(localisation);
         article.appendChild(accroche);
         article.appendChild(tarif);
-        return (article);
+
+        return (lien);
     }
-    return { name, picture, city, country, tagline, price, getUserCardDOM }
+    return { id, name, picture, city, country, tagline, price, getUserCardDOM }
 }
