c136456cef4882e5870c43c47a658defd5b87458
badgohan <kaori972@hotmail.fr>
2023-12-13 15:00:20 +0100

    Affichage des données des photographes sur la page d'acceuil

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,4 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,18 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const localisation = document.createElement('p');
+        localisation.textContent = `${city}, ${country}`;
+        const accroche = document.createElement('p');
+        accroche.textContent = tagline;
+        const tarif = document.createElement('p');
+        tarif.textContent = `${price}€/jour`
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(localisation);
+        article.appendChild(accroche);
+        article.appendChild(tarif);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
