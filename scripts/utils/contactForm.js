c179921b00c510f80566270c59701e94e5fe23d5
badgohan <kaori972@hotmail.fr>
2024-05-25 12:01:21 +0200

    Ajout de l'événement au click

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,7 +1,13 @@
+const modalBtn = document.getElementById("contact_header");
+
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 }
+
+modalBtn.forEach((btn) => btn.addEventListener("click", displayModal));
+
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
